12ddbef39c0d95185b30b92a99623a92d91a242b
Mohd Shohaib <mohdshohaibchouhan@gmail.com>
2024-07-01 17:25:00 +0530

    search

--- app/components/ProductComponent.js
+++ app/components/ProductComponent.js
@@ -3,28 +3,35 @@ import React from "react";
 import keychain from "../../public/imgs/keychain.png";
 import Rating from "./Rating";
 import { FavoriteRounded } from "@mui/icons-material";
+import { BASEURL } from "../services/http-Pos";
 
-const ProductComponent = ({ flex_direction }) => {
+const ProductComponent = ({ flex_direction, data }) => {
   return (
-    <div
-      className={`border-[1px]  p-4 border-gray-400 rounded-xl flex ${
-        flex_direction === "row"
-          ? "flex-row items-center h-full w-full"
-          : "flex-col"
-      } max-w-[400px] `}
-    >
-      <div className="relative w-full h-[200px]">
-        <Image src={keychain} layout="fill" objectFit="cover" alt="Lanyard" />
-        <button className="bg-light p-2 rounded-full absolute top-2 right-2 text-red-600">
-          <FavoriteRounded fontSize="medium" />
-        </button>
-      </div>
-      <div className={`${flex_direction === "row" ? "ml-4" : "mt-4"}`}>
-        <h2 className="product-title text-primary">Badge Reels</h2>
-        <p className="priceTitle">Rs. 168</p>
-        <Rating size={"sm"} />
-      </div>
-    </div>
+    <>
+      {data?.map((item, index) => (
+        <div
+          key={index}
+          className={`border-[1px] p-4 border-gray-400 rounded-xl flex ${
+            flex_direction === "row"
+              ? "flex-row items-center h-full w-full"
+              : "flex-col"
+          } max-w-[400px]`}
+        >
+          <div className="relative w-full h-[200px]">
+            <Image   src={`${BASEURL.ENDPOINT_URL}item/get-image/${item && item.item_id}`}
+          alt={item.name}layout="fill" objectFit="cover"  />
+            <button className="bg-light p-2 rounded-full absolute top-2 right-2 text-red-600">
+              <FavoriteRounded fontSize="medium" />
+            </button>
+          </div>
+          <div className={`${flex_direction === "row" ? "ml-4" : "mt-4"}`}>
+            <h2 className="product-title text-primary">   {item.item_name}</h2>
+            <p className="priceTitle"> ₹{item.price}</p>
+            <Rating size={"sm"} />
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
--- app/services/requestApi.js
+++ app/services/requestApi.js
@@ -5,7 +5,12 @@ class DataService {
   Login(data) {
     return https.post("auth/user-login", data);
   }
-
+  GetAllCateogary(saasId,storeId,){
+    return https.get(`category/get-list/${saasId}/${storeId}`)
+  }
+  GetDataByCatorya(saasId,storeId,category_name, currentPage){
+    return https.get(`item/get-category-list/${saasId}/${storeId}/${category_name}/${currentPage}`)
+  }
   
 }
 export default new DataService();
